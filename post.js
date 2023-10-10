799171d559aa4e33059db6020a29d238c03d9cbc
nomayen <nomayen.ohin@gmail.com>
2023-10-10 13:42:20 +0600

    post api called

---
+++ post.js
@@ -0,0 +1,24 @@
+function loadPost(){
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(data =>displayPost(data))
+}
+
+function displayPost(posts){
+        // console.log(data);
+        const postContainer = document.getElementById('posts-container')
+    for (const post of posts) {
+        console.log(post);
+        const postDiv = document.createElement('div')
+        postDiv.classList.add('post')
+        postDiv.innerHTML = `
+            <h4>user: ${post.userId}</h4>
+            <h4>Post: ${post.title}</h4>
+            <p>Post Description: ${post.body}</p>
+        
+        `;
+        postContainer.appendChild(postDiv);
+    }
+}
+
+loadPost();
