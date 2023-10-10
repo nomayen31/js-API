fbd259ff8441bf9c50ca4541df1fc83d75a6e9eb
nomayen <nomayen.ohin@gmail.com>
2023-10-10 11:15:00 +0600

    fetch data

---
+++ fetch.js
@@ -0,0 +1,11 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then(res => res.json())
+// .then(json => console.log(json))
+
+
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(json => console.log(json))
+    
+}
